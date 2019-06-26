75f8996953ebe7150649ed93beeb689b3abfd9f8
fantasticL <835899099@qq.com>
2019-06-26 17:35:53 +0800

    demo

---
+++ server/src/main/java/socool/MainLauncher.java
@@ -0,0 +1,17 @@
+package socool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by administrator_cheng on 2019/3/26.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class MainLauncher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainLauncher.class, args);
+    }
+}
